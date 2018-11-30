449d7466a6a69c10b7ad211b887756c296eaa3ed
Koychian Saeteurn <kosatun@aol.com>
2018-11-29 19:53:23 -0800

    added a form and created a 'new' action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,29 @@
+
+get '/people/new' do
+  erb :"/people/new"
+end
+
 get '/people' do
-    @people = Person.all
-    erb :"/people/index"
+  @people = Person.all
+  erb :"/people/index"
+end
+
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birthdate_string = @person.birthdate.strftime("%m%d%Y")
+  birth_path_num = Person.get_birth_path_num(birthdate_string)
+  @message = Person.get_message(birth_path_num)
+  erb :"/people/show"
+end
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate])
+
+  redirect "/people/#{person.id}"
 end

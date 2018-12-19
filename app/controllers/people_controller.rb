a0be43fcc7a9ca276e578755b318bd68a9a78921
Koychian Saeteurn <kosatun@aol.com>
2018-12-19 00:03:44 -0800

    added css, background image, and error output to app.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -24,9 +24,18 @@ post '/people' do
   else
     birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
   end
-  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate])
 
-  redirect "/people/#{person.id}"
+  person = Person.new(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @errors = ''
+    @person.errors.full_messages.each do |message|
+      @errors = "#{@errors} #{message}."
+    end
+    erb :"/people/new"
+  end
 end
 
 get '/people/:id/edit' do
@@ -35,14 +44,25 @@ get '/people/:id/edit' do
 end
 
 put '/people/:id' do
-  person = Person.find(params[:id])
-  person.first_name = params[:first_name]
-  person.last_name = params[:last_name]
-  person.birthdate = params[:birthdate]
-  person.save
-  redirect "/people/#{person.id}"
+  @person = Person.find(params[:id])
+  @person.first_name = params[:first_name]
+  @person.last_name = params[:last_name]
+  @person.birthdate = params[:birthdate]
+  @person.save
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @errors = ''
+    @person.errors.full_messages.each do |message|
+      @errors = "#{@errors} #{message}."
+    end
+    erb :"/people/edit"
+  end
 end
 
 delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.delete
   redirect "/people"
 end
